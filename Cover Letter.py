44c2e7075c4cebddcec4ec5f59e4fb79ee2db3c0
HOSSAM YOSRI <94081729+hossamyosri@users.noreply.github.com>
2024-01-22 13:50:48 +0200

    1Cover Letter.py

--- Cover Letter.py
+++ Cover Letter.py
@@ -1,28 +1,11 @@
-me="Hossam Yosri"
+me=input("Enter your name: ").title()
+Phone=input("Enter Your Phone Number: ")
 job=input("Enter the job you want: ").title()
 urfild=input("Enter your field or track about the job: " ).upper()
 Employer_Name=input("What is Employer's Name? " ).capitalize()
 Company=input("what is the Company Name? " ).upper()
 print()
 print("##"*50)
-
-#p8=f"Dear {Employer_Name}, \n   Hope this mail finds you well , \n I am interested in applying for {job} as I believe my substantial experience in {urfild}, I believe that I can make a significant contribution to your organization. \n In particular,I have experience in the field of information technology for 2 years, as it is my specialty, and I have experience in networking, network security, dealing with operating systems such as Windows and Linux, discovering and fixing problems, controlling servers, users, and permissions, securing and encrypting data and firewall like (FortiGate and Palo Alto), and dealing with Microsoft 365 programs in its various versions. \n My CV is attached, providing further information on how my career background meets your requirements. I would welcome the opportunity to meet with you in person to discuss this further. \n  I look forward to hearing from you.\n Regards: \n Hossam Yosri \n Tel: 01026214384  "
-#print(p8)
-
 #                                                                   for IT HELP DESK MORE 
-p1=f"Dear {Employer_Name}, \n  I hope this mail finds you well My name is {me} and I am writing this letter to express my interest in applying for the {job} job , as I believe that I possess everything necessary to execute the role and all associated duties while coming to you with a proactive approach and exceptional work ethic. \n First and foremost, I am the University of Matrouh graduate with a bachelor's degree in Information Technology & Computer Science  . At the university, I was among the top 2% of students with the best academic results and I attended the necessary training and tests to be a specialist in this field and the rest of the details and certificates are in my CV , I worked in student activism. This tremendous experience enabled me to become a great leader and helped me acquire exceptional analytical skills and the critical ability to work well both independently and in fast-paced group environments.  \n  In addition, I am a native Arabic speaker, fluent in English and  basic knowledge of French. After I practiced and trained as {job} in different places and Training, for over three years, I am proficient in using all the software required for this role. There, I spend most of my time providing technical support and assistance to the company's customers, solving any IT-related issues, and maintaining various hardware and software. Furthermore, I: \n  1. Train and supervise new employees and maintain the security and smoothness of the network and system. \n 2. Complete weekly reports and data and configure network devices.  \n  3. Carrying out various administrative tasks assigned by the supervisor. \n Finally, I would appreciate the opportunity to come in for an interview to discuss the position and my qualities in more detail in person. \n Thank you for your time and consideration and I look forward to hearing back from you soon. \n Kind regards, \n Hossam Yosri \n Phone: 01026214384   "
-
+p1=f"Dear {Employer_Name}, \n  I hope this mail finds you well My name is {me} and I am writing this letter to express my interest in applying for the {job} job , as I believe that I possess everything necessary to execute the role and all associated duties while coming to you with a proactive approach and exceptional work ethic. \n First and foremost, I am the University of Matrouh graduate with a bachelor's degree in Information Technology & Computer Science  . At the university, I was among the top 2% of students with the best academic results and I attended the necessary training and tests to be a specialist in this field and the rest of the details and certificates are in my CV , I worked in student activism. This tremendous experience enabled me to become a great leader and helped me acquire exceptional analytical skills and the critical ability to work well both independently and in fast-paced group environments.  \n  In addition, I am a native Arabic speaker, fluent in English and  basic knowledge of French. After I practiced and trained as {job} in different places and Training, for over three years, I am proficient in using all the software required for this role. There, I spend most of my time providing technical support and assistance to the company's customers, solving any IT-related issues, and maintaining various hardware and software. Furthermore, I: \n  1. Train and supervise new employees and maintain the security and smoothness of the network and system. \n 2. Complete weekly reports and data and configure network devices.  \n  3. Carrying out various administrative tasks assigned by the supervisor. \n Finally, I would appreciate the opportunity to come in for an interview to discuss the position and my qualities in more detail in person. \n Thank you for your time and consideration and I look forward to hearing back from you soon. \n Kind regards, \n {me} \n Phone: {phone} "
 print(p1)
-
-'''
-me="Hossam Yosri"
-myadd="Marsa Matrouh,Egypt,51511"
-email="hossamyousry907@gmail.com"
-phone='+201026214384'
-Date=input("what is the date of this day ? " )
-Employer_Name=input("What is Employer's Name? " )
-Company=input("what is the Company Name? " )
-print("         ")
-print(20*"# # #")
-print("         ")
-'''
